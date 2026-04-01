8e6e9d6dd8b33cc3fd856ed39df90d2909f1a500
alharithalk <kindaking99@gmail.com>
2026-04-01 16:30:18 +0400

    added snake location

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         // The Slithering Code Challenge [Evaluation]
+        System.out.println("------------------------------");
+        System.out.println("---------Game Started---------");
+        System.out.println("------------------------------");
 
         ArrayList<String[]> grid = new ArrayList<>();
 
@@ -32,6 +34,8 @@ public class Main {
                 mapArray[i][j] = grid.get(i)[j];
             }
         }
+        System.out.println("number of rows is : "+rows);
+        System.out.println("number of coulmn is : "+cols);
 
 
         for (int i = 0; i < rows; i++) {
@@ -41,8 +45,20 @@ public class Main {
             System.out.println();
         }
 
+        Queue <int[]>snake =new LinkedList<>();
+
+        for(int row=0;row<mapArray.length;row++){
+            for(int col=0;col<mapArray[0].length;col++) {
+                if (mapArray[col][row].equals("o")) {
+                    snake.add(new int[]{row, col});
+                }
+            }
+        }
 
 
+        for(int[] print:snake){
+            System.out.println(Arrays.toString(print) +" ");
+        }
 
 
 
